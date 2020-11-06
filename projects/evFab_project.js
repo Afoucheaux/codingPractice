02692430aeea8cacddbe7c0d5a633c2869fe00b2
Aaron Foucheaux <afoucheaux@gmail.com>
2020-11-06 13:07:47 -0600

    move euler.3x5 to dir projects

---
+++ projects/evFab_project.js
@@ -0,0 +1,20 @@
+// Even fibonacci numbers
+
+function evFibNum() {
+  var a = 1;
+  var b = 2;
+  var numbArray = [];
+
+  for (var i = 0; i <= 30; i++) {
+     // a + b = c => b = a, c = b.
+     var c = a + b;
+     var a = b;
+     var b = c;
+     if (c % 2 === 0)
+     numbArray.push(c);
+  }
+
+   console.log(numbArray);
+}
+
+evFibNum();
