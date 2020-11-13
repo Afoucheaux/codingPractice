73b8822854464655918f685de15fb0da43b769b0
Aaron Foucheaux <afoucheaux@gmail.com>
2020-11-13 12:02:31 -0600

    initial commit, checkerBoard project

---
+++ projects/checkerBoard.js
@@ -0,0 +1,27 @@
+//Checkerboard project.
+function checkerBoard(number) {
+
+  for (var i = 1; i <= number; i++) {
+    var rowArray = [];
+
+    if (i % 2 === 0) {
+      for (var j = 1; j <= number; j++) {
+        if (j % 2 === 0) {
+          rowArray.push(" ");
+        } else {
+          rowArray.push("x");
+        }
+      }
+    } else {
+        for (var j = 1; j <= number; j++) {
+          if (j % 2 === 0) {
+            rowArray.push("x");
+        } else {
+          rowArray.push(" ");
+        }
+      }
+    }
+    console.log(rowArray.join());
+  }
+}
+checkerBoard(12);
